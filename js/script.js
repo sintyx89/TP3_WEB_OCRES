c114e46daff2b5b12cf70036b018b4d279c06932
sintyx89 <43741356+sintyx89@users.noreply.github.com>
2021-09-28 16:23:24 +0200

    étape 3

--- js/script.js
+++ js/script.js
@@ -29,3 +29,36 @@ function start() {
       console.error(error);
     });
 }
+function actualise() {
+  var city;
+  // on récupère le nom de la ville
+  city = document.getElementById("city-input").value;
+
+  // Création de l'objet apiWeather
+  const apiWeather = new API_WEATHER(city);
+  // Appel de la fonction fetchTodayForecast
+
+  apiWeather
+    .fetchTodayForecast()
+    .then(function(response) {
+      // Récupère la donnée d'une API
+      const data = response.data;
+
+      // On récupère l'information principal
+      const main = data.weather[0].main;
+      const description = data.weather[0].description;
+      const temp = data.main.temp;
+      const icon = apiWeather.getHTMLElementFromIcon(data.weather[0].icon);
+
+      // Modifier le DOM
+      document.getElementById('today-forecast-main').innerHTML = main;
+      document.getElementById('today-forecast-more-info').innerHTML = description;
+      document.getElementById('icon-weather-container').innerHTML = icon;
+      document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
+      
+    })
+    .catch(function(error) {
+      // Affiche une erreur
+      console.error(error);
+    });
+}
